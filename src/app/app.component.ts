b10519f049f06c62ae5759523f70216cd7a89070
Felipe dos S. Assis <felipesntsassis@gmail.com>
2018-03-21 08:10:10 -0300

    Estruturação da página inicial e inclusão de bibliotecas.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Task Test';
+
+  public ngOnInit() {
+    $(document).ready(function () {
+      console.log("JQuery started!");
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
